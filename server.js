3c87e77cd68b214404b28d4552a3163327173675
Alexander Harris <alexanderio@Alexanders-MacBook-Pro.local>
2020-03-20 16:02:13 -0500

    relocate styles

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/app.js', (req, res) => {
   res.sendFile(__dirname + '/public/app.js')
 })
 
+app.get('/styles.css', (req, res) => {
+  res.sendFile(__dirname + '/public/styles.css')
+})
+
 app.get('/material-components-web.css', (req, res) => {
   res.sendFile(__dirname + '/node_modules/material-components-web/dist/material-components-web.css')
 })
